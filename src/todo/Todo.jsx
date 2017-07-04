b76a529353233db18a3ce9f25ebf2ae9641aaccf
Diego Dulval <deigodifreitas@gmail.com>
2017-07-04 16:40:11 -0300

    Refatoração #2

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,79 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-import Api from '../main/Api'
 import PageHeader from '../template/PageHeader'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 
-export default class Todo extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = { description: '', list: [] }
-        this.handleAdd = this.handleAdd.bind(this)
-        this.handleChange = this.handleChange.bind(this)
-        this.handleRemove = this.handleRemove.bind(this)
-        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
-        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
-        this.handleSearch = this.handleSearch.bind(this)
-        this.handleClear = this.handleClear.bind(this)
-
-        this.refresh()
-    }
-
-    handleAdd() {
-        const description = { description: this.state.description, done: false }
-        Api.createTodo(description)
-            .then(res => this.refresh())
-    }
-    handleRemove(todo) {
-        Api.deleteTodo(todo)
-            .then(res => this.refresh(this.state.description))
-    }
-    handleChange(e) {
-        this.setState({ ...this.state, description: e.target.value })
-    }
-    handleMarkAsDone(todo) {
-        todo.done = true
-        Api.alterTodo(todo)
-            .then(res => this.refresh(this.state.description))
-    }
-    handleMarkAsPending(todo) {
-        todo.done = false
-        Api.alterTodo(todo)
-            .then(res => this.refresh(this.state.description))
-    }
-    handleSearch() {
-        this.refresh(this.state.description)
-    }
-    handleClear() {
-        this.refresh()
-    }
-
-    refresh(description = '') {
-        const search = description ? `?q=${description}` : ''
-        Api.getTodos(search)
-            .then(res => {
-                this.setState({ ...this.state, description, list: res.data })
-            })
-    }
-
-    render() {
-        return (
-            <div>
-                <PageHeader name='Todo' small='Cadastro' />
-                <TodoForm
-                    description={this.state.description}
-                    handleAdd={this.handleAdd}
-                    handleChange={this.handleChange}
-                    handleSearch={this.handleSearch} 
-                    handleClear={this.handleClear}/>
-                <TodoList
-                    list={this.state.list}
-                    handleMarkAsPending={this.handleMarkAsPending}
-                    handleMarkAsDone={this.handleMarkAsDone}
-                    handleRemove={this.handleRemove} />
-            </div>
-        )
-    }
-}
+export default props => (
+    <div>
+        <PageHeader name='Todo' small='Cadastro' />
+        <TodoForm />
+        <TodoList />
+    </div>
+)
--- src/todo/todoActions.js
+++ src/todo/todoActions.js
@@ -6,18 +6,24 @@ export const changeDescription = event => ({
 })
 
 export const search = () => {
-    const request = Api.getTodos('')
-    return {
-        type: 'TODO_SEARCHED',
-        payload: request
+    return (dispatch, getState) => {
+        const description = getState().todo.description
+        const search = description ? `?q=${description}` : ''
+        const request = Api.getTodos(search)
+            .then(resp => dispatch({ type: 'TODO_SEARCHED', payload: resp.data }))
     }
 }
 
 export const add = (description) => {
-    return dispatch => {
-        Api.createTodo( { description } )
-            .then( resp => dispatch(clear()))
-            .then( resp => dispatch(search()))
+    if (!description) {
+        return dispatch => {
+            dispatch(search())
+        }
+    } else {
+        return dispatch => {
+            Api.createTodo({ description })
+                .then(resp => dispatch(clear()))
+        }
     }
 }
 
@@ -25,24 +31,24 @@ export const markAsDone = (todo) => {
     return dispatch => {
         Api.alterTodo({ ...todo, done: true })
             //.then( resp => dispatch({ type: 'TODO_MARKED_AS_DONE', payload: resp.data }))
-            .then( resp => dispatch(search()))
+            .then(resp => dispatch(search()))
     }
 }
 
 export const markAsPending = (todo) => {
     return dispatch => {
         Api.alterTodo({ ...todo, done: false })
-            .then( resp => dispatch(search()))
+            .then(resp => dispatch(search()))
     }
 }
 
 export const remove = (todo) => {
     return dispatch => {
         Api.deleteTodo(todo)
-            .then( resp => dispatch(search())) 
+            .then(resp => dispatch(search()))
     }
 }
 
 export const clear = () => {
-    return { type: 'TODO_CLEAR'}
+    return [{ type: 'TODO_CLEAR' }, search()]
 }
