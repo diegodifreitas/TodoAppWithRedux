73235e772bc875a59c844259880d6e42ba81185d
Diego Dulval <diegodifreitas@gmail.com>
2017-07-04 10:46:07 -0300

    Configurando Redux

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -2,6 +2,21 @@ import {combineReducers} from 'redux'
 
 const rooReducer = combineReducers({
     todo: () => ({
-        
+        description: "Ler Livro",
+        list: [{
+            id:1,
+            description: 'Pagar fatura do cartão',
+            done: true
+        }, {
+            id:2,
+            description: 'Reunião com a equipe as 10:00H',
+            done: false
+        }, {
+            id:3,
+            description: 'Consulta médica na terça',
+            done: false
+        }]
     })
 })
+
+export default rooReducer
